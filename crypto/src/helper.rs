4311b6cb5d5773dd69817d8c4837aeed263412b8
Moritz Eck <moritz.eck@gmail.com>
2020-11-17 12:21:53 +0100

    ✨ (crypto): add input_hashing + get_generators

--- crypto/src/helper.rs
+++ crypto/src/helper.rs
@@ -1,7 +1,8 @@
-use crate::types::{ElGamalParams, PrivateKey, PublicKey};
+use crate::types::{ElGamalParams, ModuloOperations, PrivateKey, PublicKey};
 use alloc::vec::Vec;
+use blake2::{Blake2b, Digest};
 use num_bigint::BigUint;
-use num_traits::One;
+use num_traits::{One, Zero};
 
 pub struct Helper;
 
@@ -53,11 +54,45 @@ impl Helper {
         unimplemented!()
     }
 
-    pub fn get_generator_candidates(_p: &BigUint) -> Vec<BigUint> {
-        // 1. step: find q for the given p
-        // 2. step: get all primitive roots for q
-        // 3. step: check that g is a valid generator
-        unimplemented!()
+    /// Uses the Blak2 hash function and produces a hash of four different inputs.
+    ///
+    /// The result is returned as a BigUint.
+    pub fn blake2_to_biguint(id: usize, constant: &str, i: usize, x: BigUint) -> BigUint {
+        let mut hasher = Blake2b::new();
+        let hash = hasher
+            .chain(id.to_be_bytes())
+            .chain(constant)
+            .chain(i.to_be_bytes())
+            .chain(x.to_bytes_be())
+            .finalize();
+        BigUint::from_bytes_be(&hash)
+    }
+
+    /// Returns {number} independent generators of G_q ∈ Z*_p.
+    ///
+    /// The algorithm is an adaption of the NIST standard FIPS PUB 186-4 (Appendix A.2.3)
+    pub fn get_generators(electionId: usize, q: &BigUint, number: usize) -> Vec<BigUint> {
+        let mut generators: Vec<BigUint> = Vec::new();
+        for i in 0..number {
+            let zero = BigUint::zero();
+            let one = BigUint::one();
+            let two = BigUint::from(2u32);
+
+            // start
+            let mut x = zero.clone();
+
+            let mut h_i = zero.clone();
+            while h_i == zero || h_i == one {
+                x = x + one.clone();
+
+                // hash all inputs and transform to a biguint
+                h_i = Self::blake2_to_biguint(electionId, "ggen", i, x.clone());
+                h_i = h_i % q;
+                h_i = h_i.modpow(&two, q);
+            }
+            generators.push(h_i);
+        }
+        generators
     }
 }
 
@@ -66,6 +101,7 @@ mod tests {
     use super::Helper;
     use crate::types::ElGamalParams;
     use num_bigint::BigUint;
+    use num_traits::{One, Zero};
 
     #[test]
     fn it_should_create_system() {
@@ -129,5 +165,33 @@ mod tests {
 
         let g_is_not_a_generator = Helper::is_generator(&test_params);
         assert!(g_is_not_a_generator);
+    }
+
+    #[test]
+    fn it_should_hash_and_return_biguint() {
+        let id: usize = 1;
+        let constant = "ggen";
+        let mut i: usize = 1;
+        let x = BigUint::one();
+        let hash1 = Helper::blake2_to_biguint(id, constant, i, x.clone());
+
+        i = 2;
+        let hash2 = Helper::blake2_to_biguint(id, constant, i, x);
+
+        let one = BigUint::one();
+        assert!(hash1 > one.clone());
+        assert!(hash2 > one);
+        assert_ne!(hash1, hash2);
+    }
+
+    #[test]
+    fn it_should_get_generators() {
+        let one = BigUint::one();
+        let id: usize = 1;
+        let (params, sk, pk) = Helper::setup_system(b"23", b"2", b"4");
+
+        let generators = Helper::get_generators(id, &params.q(), 10);
+        assert_eq!(generators.len(), 10);
+        assert!(generators.iter().all(|value| value.clone() > one));
     }
 }
