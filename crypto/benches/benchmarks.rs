2f5dbeb6bd649d21c60a2dd7c631faa6eba9299b
Moritz Eck <moritz.eck@gmail.com>
2020-12-09 22:12:34 +0100

    adds shuffling benchmarks

--- crypto/benches/benchmarks.rs
+++ crypto/benches/benchmarks.rs
@@ -1,12 +1,61 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use crypto::{encryption::ElGamal, helper::Helper};
+use crypto::{
+    encryption::ElGamal,
+    helper::Helper,
+    types::{Cipher, PublicKey},
+};
 use num_bigint::BigUint;
 use num_traits::{One, Zero};
+use std::time::Duration;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    // benchmark congig
+fn setup_shuffling(nr_of_votes: usize) -> (Vec<Cipher>, Vec<usize>, Vec<BigUint>, PublicKey) {
+    let (_, _, pk) = Helper::setup_lg_system();
+    let q = pk.params.q();
+
+    // encryption of zero
+    let zero = BigUint::zero();
+    let r = BigUint::parse_bytes(b"1234", 10).unwrap();
+    let enc_zero = ElGamal::encrypt(&zero, &r, &pk);
+
+    // encryption of one
+    let one = BigUint::one();
+    let r_ = BigUint::parse_bytes(b"4321", 10).unwrap();
+    let enc_one = ElGamal::encrypt(&one, &r_, &pk);
+
+    let mut encryptions: Vec<Cipher> = Vec::new();
+    let mut randoms: Vec<BigUint> = Vec::new();
+    let power = BigUint::parse_bytes(b"ABCDEF123456789ABCDEF123412341241241241124", 16).unwrap();
+    let mut permutation: Vec<usize> = Vec::new();
+
+    for i in 0..nr_of_votes {
+        permutation.push(i);
+
+        let mut random = BigUint::from(i);
+        random *= BigUint::from(i);
+        random = random.modpow(&power, &q);
+        randoms.push(random);
+
+        if i % 2 == 0 {
+            encryptions.push(enc_zero.clone());
+        } else {
+            encryptions.push(enc_one.clone());
+        }
+    }
+
+    // create a fake permutation
+    permutation.reverse();
+
+    assert!(encryptions.len() == randoms.len());
+    assert!(encryptions.len() == permutation.len());
+    assert!(encryptions.len() == nr_of_votes);
+    (encryptions, permutation, randoms, pk)
+}
+
+fn bench_elgamal(c: &mut Criterion) {
+    // benchmark config
     let mut group = c.benchmark_group("elgamal");
-    group.sample_size(500);
+    group.measurement_time(Duration::new(15, 0));
+    group.sample_size(350);
 
     group.bench_function("encryption", |b| {
         b.iter_with_setup(
@@ -94,41 +143,34 @@ fn criterion_benchmark(c: &mut Criterion) {
             |(encryption, r_, pk)| ElGamal::re_encrypt_via_addition(&encryption, &r_, &pk),
         )
     });
+}
 
-    group.bench_function("shuffling (permutation + re-encryption)", |b| {
+fn bench_shuffling(c: &mut Criterion) {
+    // benchmark config
+    let mut group = c.benchmark_group("shuffling");
+    group.sample_size(20);
+
+    group.bench_function("3 votes", |b| {
         b.iter_with_setup(
-            || {
-                let (_, _, pk) = Helper::setup_lg_system();
-
-                // encryption of zero
-                let zero = BigUint::zero();
-                let r = BigUint::parse_bytes(b"1234", 10).unwrap();
-                let enc_zero = ElGamal::encrypt(&zero, &r, &pk);
-
-                // encryption of one
-                let one = BigUint::one();
-                let r_ = BigUint::parse_bytes(b"4321", 10).unwrap();
-                let enc_one = ElGamal::encrypt(&one, &r_, &pk);
-
-                // encryption of two
-                let two = BigUint::from(2u32);
-                let r__ = BigUint::parse_bytes(b"2431", 10).unwrap();
-                let enc_two = ElGamal::encrypt(&two, &r__, &pk);
-
-                let encryptions = vec![enc_zero, enc_one, enc_two];
-
-                // create three random values < q
-                let randoms = vec![
-                    BigUint::parse_bytes(b"4321", 10).unwrap(),
-                    BigUint::parse_bytes(b"3142", 10).unwrap(),
-                    BigUint::parse_bytes(b"2413", 10).unwrap(),
-                ];
-
-                // create a permutation of size 3
-                let permutation = vec![2, 0, 1];
-
-                (encryptions, permutation, randoms, pk)
+            || setup_shuffling(3),
+            |(encryptions, permutation, randoms, pk)| {
+                ElGamal::shuffle(&encryptions, &permutation, &randoms, &pk)
             },
+        )
+    });
+
+    group.bench_function("30 votes", |b| {
+        b.iter_with_setup(
+            || setup_shuffling(30),
+            |(encryptions, permutation, randoms, pk)| {
+                ElGamal::shuffle(&encryptions, &permutation, &randoms, &pk)
+            },
+        )
+    });
+
+    group.bench_function("100 votes", |b| {
+        b.iter_with_setup(
+            || setup_shuffling(100),
             |(encryptions, permutation, randoms, pk)| {
                 ElGamal::shuffle(&encryptions, &permutation, &randoms, &pk)
             },
@@ -138,5 +180,5 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, bench_elgamal, bench_shuffling);
 criterion_main!(benches);
