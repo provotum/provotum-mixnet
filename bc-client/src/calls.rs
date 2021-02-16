faa0926511abd2afeafaf8a1bfcfcb0981aacb3b
Moritz Eck <moritz.eck@gmail.com>
2021-02-16 11:17:04 +0000

    feat: add more calls + refactor

--- bc-client/src/calls.rs
+++ bc-client/src/calls.rs
@@ -1,10 +1,55 @@
 use codec::Encode;
-use pallet_mixnet::types::{PublicParameters, Topic};
+use pallet_mixnet::types::{
+    Ballot, PublicKey as SubstratePK, PublicParameters, Title, Topic, VoteId, VotePhase,
+};
+use substrate_subxt::{Call, EventsDecoder, NodeTemplateRuntime};
 
 #[derive(Encode)]
-pub struct CreateVoteCall {
-    pub vote_id: Vec<u8>,
-    pub title: Vec<u8>,
+pub struct CreateVote {
+    pub vote_id: VoteId,
+    pub title: Title,
     pub params: PublicParameters,
     pub topics: Vec<Topic>,
+}
+
+impl Call<NodeTemplateRuntime> for CreateVote {
+    const MODULE: &'static str = "PalletMixnet";
+    const FUNCTION: &'static str = "create_vote";
+    fn events_decoder(_decoder: &mut EventsDecoder<NodeTemplateRuntime>) {}
+}
+
+#[derive(Encode)]
+pub struct StorePublicKey {
+    pub vote_id: VoteId,
+    pub pk: SubstratePK,
+}
+
+impl Call<NodeTemplateRuntime> for StorePublicKey {
+    const MODULE: &'static str = "PalletMixnet";
+    const FUNCTION: &'static str = "store_public_key";
+    fn events_decoder(_decoder: &mut EventsDecoder<NodeTemplateRuntime>) {}
+}
+
+#[derive(Encode)]
+pub struct SetVotePhase {
+    pub vote_id: VoteId,
+    pub vote_phase: VotePhase,
+}
+
+impl Call<NodeTemplateRuntime> for SetVotePhase {
+    const MODULE: &'static str = "PalletMixnet";
+    const FUNCTION: &'static str = "set_vote_phase";
+    fn events_decoder(_decoder: &mut EventsDecoder<NodeTemplateRuntime>) {}
+}
+
+#[derive(Encode)]
+pub struct CastBallot {
+    pub vote_id: VoteId,
+    pub ballot: Ballot,
+}
+
+impl Call<NodeTemplateRuntime> for CastBallot {
+    const MODULE: &'static str = "PalletMixnet";
+    const FUNCTION: &'static str = "cast_ballot";
+    fn events_decoder(_decoder: &mut EventsDecoder<NodeTemplateRuntime>) {}
 }
--- bc-client/src/main.rs
+++ bc-client/src/main.rs
@@ -1,29 +1,44 @@
 mod calls;
 
-use crate::calls::CreateVoteCall;
+use crate::calls::{CreateVote, SetVotePhase};
 use crypto::helper::Helper;
-use pallet_mixnet::types::{PublicParameters, Topic};
+use pallet_mixnet::types::{PublicParameters, Topic, VoteId, VotePhase};
 use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring};
-use substrate_subxt::system::AccountStoreExt;
-use substrate_subxt::{Call, ClientBuilder, Error, EventsDecoder, NodeTemplateRuntime, PairSigner};
-
-impl Call<NodeTemplateRuntime> for CreateVoteCall {
-    const MODULE: &'static str = "TemplateModule";
-    const FUNCTION: &'static str = "create_vote";
-    fn events_decoder(_decoder: &mut EventsDecoder<NodeTemplateRuntime>) {}
-}
+use substrate_subxt::{system::AccountStoreExt, Call, Client, ExtrinsicSuccess};
+use substrate_subxt::{ClientBuilder, Error, NodeTemplateRuntime, PairSigner};
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
 
+    let voting_authority_url = "ws://127.0.0.1:9944";
     let client = ClientBuilder::<NodeTemplateRuntime>::new()
-        .set_url("ws://127.0.0.1:9944")
+        .set_url(voting_authority_url)
         .build()
         .await?;
 
-    let block_number = 1;
+    // let block_number = 1;
+    // let block_hash_response = get_block_hash(&client, block_number).await;
 
+    let create_vote_response = create_vote(&client).await;
+    println!("create_vote_response: {:?}", create_vote_response);
+
+    // TODO: update vote phase to Voting
+    let vote_id = "20201212".as_bytes().to_vec();
+    let set_vote_phase_response = set_vote_phase(&client, vote_id, VotePhase::Voting).await;
+    println!("set_vote_phase_response: {:?}", set_vote_phase_response);
+
+    // TODO: submit some ballots
+
+    // TODO: update vote phase to Tallying
+
+    Ok(())
+}
+
+pub async fn get_block_hash(
+    client: &Client<NodeTemplateRuntime>,
+    block_number: u32,
+) -> Result<(), Error> {
     let block_hash = client.block_hash(Some(block_number.into())).await?;
 
     if let Some(hash) = block_hash {
@@ -34,11 +49,14 @@ async fn main() -> Result<(), Error> {
 
     let mut iter = client.account_iter(None).await?;
     while let Some((key, account)) = iter.next().await? {
-        println!("{:?}: {:#?}", key, account.data.free);
+        println!("{:?}: {:#?}", key, account);
     }
+    Ok(())
+}
 
-    let signer = PairSigner::<NodeTemplateRuntime, Pair>::new(AccountKeyring::Alice.pair());
-
+pub async fn create_vote(
+    client: &Client<NodeTemplateRuntime>,
+) -> Result<ExtrinsicSuccess<NodeTemplateRuntime>, Error> {
     // create the vote
     let (params, _, _) = Helper::setup_sm_system();
     let params: PublicParameters = params.into();
@@ -50,18 +68,33 @@ async fn main() -> Result<(), Error> {
     let topic: Topic = (topic_id, topic_question);
     let topics = vec![topic];
 
-    let response = client
-        .watch(
-            CreateVoteCall {
-                params,
-                title: vote_title,
-                vote_id,
-                topics,
-            },
-            &signer,
-        )
-        .await?;
-    println!("response: {:?}", response);
+    let signer = PairSigner::<NodeTemplateRuntime, Pair>::new(AccountKeyring::Alice.pair());
+    let create_vote_call = CreateVote {
+        params,
+        title: vote_title,
+        vote_id,
+        topics,
+    };
+    return submit(&signer, client, create_vote_call).await;
+}
 
-    Ok(())
+pub async fn set_vote_phase(
+    client: &Client<NodeTemplateRuntime>,
+    vote_id: VoteId,
+    vote_phase: VotePhase,
+) -> Result<ExtrinsicSuccess<NodeTemplateRuntime>, Error> {
+    let signer = PairSigner::<NodeTemplateRuntime, Pair>::new(AccountKeyring::Alice.pair());
+    let set_vote_phase = SetVotePhase {
+        vote_id,
+        vote_phase,
+    };
+    return submit(&signer, client, set_vote_phase).await;
+}
+
+async fn submit<C: Call<NodeTemplateRuntime> + Send + Sync>(
+    signer: &PairSigner<NodeTemplateRuntime, Pair>,
+    client: &Client<NodeTemplateRuntime>,
+    call: C,
+) -> Result<ExtrinsicSuccess<NodeTemplateRuntime>, Error> {
+    return client.watch(call, signer).await;
 }
